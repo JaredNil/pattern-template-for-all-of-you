dedf008adb0a21c9a6c198625df889d8732a4ac6
JaredN <77531492+JaredNil@users.noreply.github.com>
2022-09-18 05:40:32 +0800

    Update pattern.jsx

--- pattern.jsx
+++ pattern.jsx
@@ -1,11 +1,48 @@
-Factory
+Factory method. Объединение инициализации инстансов в одном методе с замыканием.
+{
+	function BlockFactory() { };
+
+	BlockFactory.register = function (name, ItemConstructor) { // Outer
+		if (name instanceof Function) {
+			ItemConstructor = name
+			name = null
+		}
+
+		if (!(ItemConstructor instanceof Function)) {
+			throw { name: 'Error', message: 'ItemConstructor is not function' }
+		}
+		this[name || ItemConstructor.name] = ItemConstructor
+	}
+
+	BlockFactory.create = function (itemName) {
+		let ItemConstructor = this[itemName];
+		if (!(ItemConstructor instanceof Function)) {
+			throw {
+				name: 'Error',
+				message: 'constructor "' + itemName + '" undefined'
+			}
+		}
+		return new ItemConstructor();
+	}
+
+	BlockFactory.register('dog', function () {
+
+		this.say = () => console.log('gav') // BODY-CONSTRUCTOR ITEMS FOR FACTORY
+
+	})
+
+	let dogInstanse = BlockFactory.create('dog') // create instance
+	dogInstanse.say()  // closures
+}
+
+
 
 
 Builder
 Prototype
-Singleton
+Singleton. Функция, которая создаёт один и тот же объект с неизменяемыми свойствами. Сам объект вшит в прототип.
 function SingletonFunction() {
-	let instance;
+	let instance; 
 	SingletonFunction = function () {
 		return instance;
 	}
