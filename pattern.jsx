ef94b00864a45fc1957cfb7e96571bc5ce83cfbb
JaredN <77531492+JaredNil@users.noreply.github.com>
2022-09-17 19:26:18 +0800

    singleton add

--- pattern.jsx
+++ pattern.jsx
@@ -1,8 +1,25 @@
 Factory
+
+
 Builder
 Prototype
 Singleton
+function SingletonFunction() {
+	let instance;
+	SingletonFunction = function () {
+		return instance;
+	}
+	SingletonFunction.prototype = this;
 
+	instance = new SingletonFunction();
+	instance.constructor = SingletonFunction;
+	instance.property1 = 'value';
+	instance.property2 = 'value';
+
+	return instance;
+}
+
+//////////////////////////////////////////////////////////////////////////////
 Adapter
 Brigde
 Composite
