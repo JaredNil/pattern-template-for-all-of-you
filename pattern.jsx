8a8c5ecc12625513e04fe7938ae8a6e84e57da25
JaredN <77531492+JaredNil@users.noreply.github.com>
2022-09-17 15:42:43 +0800

    pattent-info
    gg

---
+++ pattern.jsx
@@ -0,0 +1,24 @@
+Factory
+Builder
+Prototype
+Singleton
+
+Adapter
+Brigde
+Composite
+Decorator
+Fasade
+Flyweight
+Proxy
+
+Chain
+Command
+Iterpraton
+Iteration
+Mediator
+Memento
+Observer
+State
+Stronger
+template method
+Visitor
